8606ea733d38d7cc71cf7a9e59e21a806123e8c4
chrisdarragh11 <85189003+chrisdarragh11@users.noreply.github.com>
2021-06-24 14:13:40 -0400

    Update craps.py

--- craps.py
+++ craps.py
@@ -1,11 +1,30 @@
-def game():
-    print("Welcome to Craps, enter R to roll")
+import random
+def craps():
+    print("Welcome to Craps, enter R to roll for Player 1")
     userinput = input()
-    if (userinput == "R"):
+    if (userinput == "R") or (userinput == "r"):
         dice = random.randint(1,6)
         dice2 = random.randint(1,6)
         print("Dice 1: ", dice)
         print("Dice 2: ", dice2)
     else:
         print("Input not recognized, try again")
+    dicesum1 = dice + dice2
+    print("Enter R to roll for Player 2")
+    userinput2 = input()
+    if (userinput2 == "R") or (userinput2 == "r"):
+        dice3 = random.randint(1,6)
+        dice4 = random.randint(1,6)
+        print("Dice 1: ", dice3)
+        print("Dice 2: ", dice4)
+    else:
+        print("Input not recognized, try again")
+    dicesum2 = dice3 + dice4
+    if dicesum1 > dicesum2:
+        print("Player 1 wins!")
+    elif dicesum2 > dicesum1:
+        print("Player 2 wins!")
+    elif dicesum1 == dicesum2:
+        print("Tie!")
 
+craps()

c9c85338de3be6b8ac7436d487446035c2d3af93
Program-Rosado <85189060+Program-Rosado@users.noreply.github.com>
2021-07-27 12:18:10 -0400

    Update GameData and PlayerData

--- CasinoBackEnd/gamedata.py
+++ CasinoBackEnd/gamedata.py
@@ -8,6 +8,7 @@ class GamePrefixID(Enum):
     # Fill in more games
 
 
+<<<<<<< Updated upstream
 class GameData():
     PrefixID: GamePrefixID
 
@@ -16,3 +17,92 @@ class GameData():
 
 class G_BlackJack(GameData):
     PrefixID = GamePrefixID.BlackJack
+=======
+
+    def set_amountWon(self, new_amountWon):
+        # set the two ammount attributes
+        self.amountWon = new_amountWon
+
+    def set_amountLost(self, new_amountLost):
+        # set the two ammount attributes
+        self.amountLost = new_amountLost
+
+    def set_date(self, new_date):
+        # set date attributes
+        self.date = new_date
+
+    def get_data_from_DB(self):
+        # retrive amount won,  amount lost, and date in MatchData table by matching self.ID with MID
+        self.sql.select_from_where("MatchData", "Won", "MID", self.matchID)
+        self.sql.select_from_where("MatchData", "Lost", "MID", self.matchID)
+        self.sql.select_from_where("MatchData", "Date", "MID", self.matchID)
+
+    def update_data_to_DB(self):
+        # Find the difference between class amount won and lost VS stored amount won and lost in the DB
+        # then update the amounts appropriately to the MatchData, PlayerFinance, and GameMain tables
+        # GID is the first 3 digits of MID
+        stored_Won = self.sql.select_from_where("MatchData", "Won", "MID", self.matchID)
+        stored_Lost = self.sql.select_from_where("MatchData", "Lost", "MID", self.matchID)
+        
+        if stored_Won == 0 and stored_Lost == 0:
+            self.sql.update_set_where("MatchData", "Won", "PID", self.amountWon)
+            self.sql.update_set_where("MatchData", "Lost", "PID", self.amountLost)
+            self.sql.update_set_where("MatchData", "Date", "PID", self.date)
+
+
+class GameData:
+    PrefixID: GamePrefixID
+    
+    def __init__(self, gameID) -> None:
+        self.gameID = gameID
+        self.totalPlayerWon = 0
+        self.totalPlayerLost = 0
+
+    def set_amount_won(self, amountWon):
+        # set to class attribute
+        self.totalPlayerWon = amountWon
+
+    def set_amount_lost(self, amountLost):
+        # set to class attribute
+        self.totalPlayerLost = amountLost
+
+    def get_amount_from_DB(self):
+        # from GameMain
+        self.sql.select_from_where("GameMain", "TotalPlayerWon", "GID", self.gameID)
+        self.sql.select_from_where("GameMain", "TotalPlayerLost", "GID", self.gameID)
+        
+        
+    
+class G_BlackJack(MatchData, GameData):
+    """Black Jack- try to get 21, number cars = value ace can be 1 or 11 face cards are 10, bust if hit over 21"""
+    PrefixID = GamePrefixID.BlackJack
+
+class G_Craps(MatchData, GameData):
+    """Craps- """
+    PrefixID = GamePrefixID.Craps
+
+class G_Roulette(MatchData, GameData):
+    """Roulette"""
+    PrefixID = GamePrefixID.Roulette
+
+class G_Slots(MatchData, GameData):
+    """Slots"""
+    PrefixID = GamePrefixID.Slots
+
+class G_Keno(MatchData, GameData):
+    """Keno- Player pick 20 numbers fro 1-80, numbers picked payout on how many right numbers picked """
+    PrefixID = GamePrefixID.Keno
+
+class G_Poker(MatchData, GameData):
+    """Poker"""
+    PrefixID = GamePrefixID.Poker
+
+class G_Baccarat(MatchData, GameData):
+    """Baccarat- played against dealer 2-9 cards are value ace is 1, rest=0, add hand take ones place number as score 0-9"""
+    PrefixID = GamePrefixID.Baccarat
+
+class G_BigSix(MatchData, GameData):
+    """Big Six, Wheel of Fortune- """
+    PrefixID = GamePrefixID.BigSix
+    
+>>>>>>> Stashed changes
--- CasinoBackEnd/playerdata.py
+++ CasinoBackEnd/playerdata.py
@@ -1,6 +1,13 @@
 class PlayerData:
+<<<<<<< Updated upstream
     def __init__(self, ID, lastname, firstname, balance, won, lost) -> None:
         self._ID = ID
+=======
+    def __init__(self, PID, lastname = "TBU", firstname = "TBU", balance = 0, won = 0, lost = 0):
+        # TBU: to be updated
+        
+        self._ID = PID
+>>>>>>> Stashed changes
         self._lastname = lastname
         self._firstname = firstname
         self._balance = balance
@@ -8,6 +15,7 @@ class PlayerData:
         self._lost = lost
 
 
+<<<<<<< Updated upstream
     def set_ID(self, new_ID): 
         # Set ID number (int) here ex.
         self._ID = new_ID        
@@ -33,3 +41,98 @@ class PlayerData:
     def get_finance(self):
         # Return List of [Balance, Won, Lost]
         pass
+=======
+#     def set_ID(self, new_ID): 
+#         # Set ID number (int) into temp. constructor ex.
+#         self._ID = new_ID        
+
+# # <<<<<<< HEAD
+#     def set_last_name(self, new_lastname):
+#         # Set Last Name (str) into temp. constructor ex.
+#         self._lastname = new_lastname       
+    
+
+#     def set_first_name(self, new_firstname):
+#         # Set First Name (str) into temp. constructor ex.
+#         self._firstname = new_firstname       
+        
+# # =======
+#     def set_name(self, new_lastname, new_firstname):
+#         # Set Last Name (str) here ex.
+#         self._lastname = new_lastname
+#         self._firstname = new_firstname
+
+# # >>>>>>> 6a45be68a39b70934de3e78896f6f17973f2d685
+
+#     def set_finance(self, new_balance, new_won, new_lost):
+#         # Set Balance, Win, and Lost numbers (int, int, int) into temp. constructor ex.
+#         self._balance = new_balance
+#         self._won = new_won
+#         self._lost = new_lost       
+
+    def update_ID_to_db(self, new_ID):
+        # pulls ID from temp player and updates the db with the temp ID
+        condtn = f"PID = \'{self._ID}\'"
+        self.sql.update_set_where("PlayerMain", new_ID, condtn) 
+        self.sql.update_set_where("PlayerFinance", new_ID, condtn) 
+        self.sql.update_set_where("Cheater", new_ID, condtn) 
+        self.sql.update_set_where("MatchData", new_ID, condtn)
+        self.sql.update_set_where("PlayerSkill", new_ID, condtn)  
+        self._ID = new_ID
+        
+
+    def update_lastname_to_db(self, new_last):
+        # pulls last name from temp player and updates the db with the temp last name
+        condtn = f"Last = \'{self._lastname}\'"
+        self.sql.update_set_where("PlayerMain", new_last, condtn)
+        self._lastname = new_last
+
+    def update_firstname_to_db(self, new_first):
+        # pulls first name from temp player and updates the db with the temp first name
+        condtn = f"First = \'{self._firstname}\'"
+        self.sql.update_set_where("PlayerMain", new_first, condtn)
+        self._firstname = new_first
+
+    def update_finance_to_db(self, new_finance):
+        # pulls balance, wins/losses from temp player and updates the db with the temp balance, wins/losses
+        condtn = f"Balance = {self._balance}"
+        self.sql.update_set_where("PlayerFinance", new_finance, condtn)
+        self._balance = new_finance
+
+    # def get_ID_from_DB(self):
+    #     # go to PlayerMain table and return PID
+    #     self.sql.select_from("PlayerMain", ID = self._ID)
+    #     # need actual table name
+
+    def get_firstname_from_DB(self):
+        # go to PlayerMain table and return Last and First
+        self.sql.select_from_where("PlayerName", "First", "PID", self._firstname)
+        
+    def get_lastname_from_DB(self):
+        # go to PlayerMain table and return Last and First
+        self.sql.select_from_where("PlayerName", "Last", "PID", self._lastname)
+
+    def get_finance_from_DB(self):
+        # go to PlayerMain table and return Last and First
+        self.sql.select_from_where("PlayerFinance", "Balance", "PID", self._balance)
+
+    # def update_ID_to_DB(self):
+    #     # use sql's UPDATE function to update PID with new self._ID  
+    #     # in the following tables
+    #     # Cheater, MatchData, PlayerFinance, PlayerMain, PlayerSkill
+    #     pass
+
+    # def update_name_to_DB(self):
+    #     # update Last and First in PlayerMain
+    #     pass
+
+    # def update_finance_to_DB(self):
+    #     # update Balance, totalLost, and totalWon in PlayerFinance
+    #     pass
+
+    def update_all(self):
+        self.update_ID_to_db()
+        self.update_firstname_to_db()
+        self.update_lastname_to_db()
+        self.update_finance_to_db()
+>>>>>>> Stashed changes

7cb2294925511851ceba97c465ba64881fd09241
HkPPP <phamh4@wit.edu>
2021-07-28 17:48:31 -0400

    added Test

--- test/test_func.py
+++ test/test_func.py
@@ -5,11 +5,6 @@ sys.path.append('../')
 import pytest
 
 from CasinoBackEnd import SQL_Database
-import unittest
 
-class TestCases(unittest.TestCase):
-
-    def testCase1(self, exp):
-        expected = [] #correct result
-        actual = [] #derived result
-        self.assertEqual(expected, actual)
+class TestCases(pytest):
+    pass

35211687dc73d60dfde05d884db86496fe258880
Program-Rosado <85189060+Program-Rosado@users.noreply.github.com>
2021-08-03 11:47:24 -0400

    Update playerskill.py

--- CasinoBackEnd/playerskill.py
+++ CasinoBackEnd/playerskill.py
@@ -11,48 +11,48 @@ class PlayerSkill:
 
         self.sql = SQL_Databases()
 
-    def update_ID_to_db(self, new_ID):
-        # pulls ID from temp player and updates the db with the temp ID
-        condtn = f"PID = \'{self._ID}\'"
-        self.sql.update_set_where("PlayerMain", new_ID, condtn) 
-        self.sql.update_set_where("PlayerFinance", new_ID, condtn) 
-        self.sql.update_set_where("Cheater", new_ID, condtn) 
-        self.sql.update_set_where("MatchData", new_ID, condtn)
-        self.sql.update_set_where("PlayerSkill", new_ID, condtn)  
-        self._ID = new_ID
+    # def update_ID_to_db(self, new_ID):
+    #     # pulls ID from temp player and updates the db with the temp ID
+    #     condtn = f"PID = \'{self._ID}\'"
+    #     self.sql.update_set_where("PlayerMain", new_ID, condtn) 
+    #     self.sql.update_set_where("PlayerFinance", new_ID, condtn) 
+    #     self.sql.update_set_where("Cheater", new_ID, condtn) 
+    #     self.sql.update_set_where("MatchData", new_ID, condtn)
+    #     self.sql.update_set_where("PlayerSkill", new_ID, condtn)  
+    #     self._ID = new_ID
 
     def update_skill_to_db(self, new_skill):
         # pulls skill from temp player and updates the db with the temp skill
-        condtn = f"Skill = \'{self._skill}\'"
+        condtn = f"Skill = {self._skill}"
         self.sql.update_set_where("PlayerSkill", new_skill, condtn)
         self._skill = new_skill
 
     def update_cheat_to_db(self, new_cheat):
         # pulls cheat from temp player and updates the db with the temp cheat
-        condtn = f"Last = \'{self._cheat}\'"
+        condtn = f"Last = {self._cheat}"
         self.sql.update_set_where("PlayerSkill", new_cheat, condtn)
         self._cheat = new_cheat
 
     def update_luck_to_db(self, new_luck):
         # pulls LUCK from temp player and updates the db with the temp LUCK
-        condtn = f"Last = \'{self._luck}\'"
+        condtn = f"Last = {self._luck}"
         self.sql.update_set_where("PlayerSkill", new_luck, condtn)
         self._luck = new_luck
 
 
     def get_skill_from_DB(self):
         # go to PlayerSkill table and return Skill
-        ID_in_quote = f"\'{self._ID}\'"
+        ID_in_quote = f"{self._ID}"
         self._skill = self.sql.select_from_where("PlayerSkill", "Skill", "PID", ID_in_quote)[0][0]
         
     def get_cheat_from_DB(self):
         # go to PlayerSkill table and return Cheat stat
-        ID_in_quote = f"\'{self._ID}\'"
+        ID_in_quote = f"{self._ID}"
         self._cheat = self.sql.select_from_where("PlayerSkill", "Cheat", "PID", ID_in_quote)[0][0]
 
     def get_luck_from_DB(self):
         # go to PlayerSkill table and return LUCK stat
-        ID_in_quote = f"\'{self._ID}\'"
+        ID_in_quote = f"{self._ID}"
         self._luck = self.sql.select_from_where("PlayerSkill", "LUCK", "PID", ID_in_quote)[0][0]
 
     

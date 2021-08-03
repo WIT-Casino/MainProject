72437d0b395d2d2b576d8ef636cd81116cce44f1
Program-Rosado <85189060+Program-Rosado@users.noreply.github.com>
2021-08-03 12:38:35 -0400

    Update gamedata.py

--- CasinoBackEnd/gamedata.py
+++ CasinoBackEnd/gamedata.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 try:
     from SQL_Database import SQL_Databases
+except ModuleNotFoundError:
     import sys
     sys.path.append(".")
     from CasinoBackEnd.SQL_Database import SQL_Databases

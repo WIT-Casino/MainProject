71bc52f3ceb1c5114987ac6f5fe7f4ae21606bf8
HkPPP <46695607+HkPPP@users.noreply.github.com>
2021-08-03 20:16:39 -0400

    Remove "\'" around int, and added single quotes for str

--- CasinoBackEnd/playerskill.py
+++ CasinoBackEnd/playerskill.py
@@ -9,9 +9,9 @@ class PlayerSkill:
     def __init__(self, ID, Skill, Cheat, LUCK) -> None:
             
         self._ID = f"\'{ID}\'"
-        self._skill = f"\'{Skill}\'"
-        self._cheat = f"\'{Cheat}\'"
-        self._luck = f"\'{LUCK}\'"
+        self._skill = f"{Skill}"
+        self._cheat = f"{Cheat}"
+        self._luck = f"{LUCK}"
 
 
         self.sql = SQL_Databases()
@@ -47,17 +47,17 @@ class PlayerSkill:
 
     def pull_skill_from_DB(self):
         """ go to PlayerSkill table and return Skill"""
-        ID_in_quote = f"{self._ID}"
+        ID_in_quote = f"'{self._ID}'"
         self._skill = self.sql.select_from_where("PlayerSkill", "Skill", "PID", ID_in_quote)[0][0]
         
     def pull_cheat_from_DB(self):
         """ go to PlayerSkill table and return Cheat stat""" 
-        ID_in_quote = f"{self._ID}"
+        ID_in_quote = f"'{self._ID}'"
         self._cheat = self.sql.select_from_where("PlayerSkill", "Cheat", "PID", ID_in_quote)[0][0]
 
     def pull_luck_from_DB(self):
         """ go to PlayerSkill table and return LUCK stat""" 
-        ID_in_quote = f"{self._ID}"
+        ID_in_quote = f"'{self._ID}'"
         self._luck = self.sql.select_from_where("PlayerSkill", "LUCK", "PID", ID_in_quote)[0][0]
 
     
